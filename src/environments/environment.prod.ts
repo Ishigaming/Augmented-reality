49b0e980d2afd9a0fb86a99e2ca4a537d5744aaa
Ishigaming <juanmar31500@gmail.com>
2025-11-24 14:43:44 -0500

    Creation

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyDbqSEG1KsRpPFJ-Hbd8ucglz7ysS0IQRs",
+  authDomain: "augmented-reality-14d5a.firebaseapp.com",
+  projectId: "augmented-reality-14d5a",
+  storageBucket: "augmented-reality-14d5a.firebasestorage.app",
+  messagingSenderId: "634032946340",
+  appId: "1:634032946340:web:2bc19b5ac54bc496b6fa31",
+  measurementId: "G-J854LK3RT8"
+}
+};
